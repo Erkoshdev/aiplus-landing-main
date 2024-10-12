86026dbce60552a1ffa1a485443f9e6bdcedc483
Erkosh <yerkebulan.safiolla@gmail.com>
2024-10-12 23:45:05 +0500

    верстка блоков slider, program, result

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+// //animations
+// jQuery(function($) {
+//
+//   // Function which adds the 'animated' class to any '.animatable' in view
+//   let doAnimations = function() {
+//
+//     // Calc current offset and get all animatables
+//     let offset = $(window).scrollTop() + $(window).height(),
+//       $animatables = $('.animatable');
+//
+//     // Unbind scroll handler if we have no animatables
+//     if ($animatables.length == 0) {
+//       $(window).off('scroll', doAnimations);
+//     }
+//
+//     // Check all animatables and animate them if necessary
+//     $animatables.each(function(i) {
+//       let $animatable = $(this);
+//       if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+//         $animatable.removeClass('animatable').addClass('animated');
+//       }
+//     });
+//   };
+//
+//   // Hook doAnimations on scroll, and trigger a scroll
+//   $(window).on('scroll', doAnimations);
+//   $(window).trigger('scroll');
+// });
